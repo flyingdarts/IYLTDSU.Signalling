97adab7e3ec1fc662aaf13d6ccabe9aa0e80bfeb
Mike Pattyn <mike.pattyn1@gmail.com>
2022-10-08 15:52:54 +0200

    Pure lambda ist nu wel

--- IYLTDSU.Signalling.Infrastructure/Endpoints.cs
+++
@@ -1,7 +0,0 @@
-﻿public struct Endpoints
-{
-    public Endpoints() { }
-    public string OnConnect { get; set; } = "OnConnect";
-    public string OnDisconnect { get; set; } = "OnDisconnect";
-    public string OnMessage { get; set; } = "OnMessage";
-}
--- IYLTDSU.Signalling.Infrastructure/Program.cs
+++ IYLTDSU.Signalling.Infrastructure/Program.cs
@@ -1,6 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Amazon.CDK;
+using Amazon.CDK.AWS.Apigatewayv2.Alpha;
+using Amazon.CDK.AWS.Apigatewayv2.Integrations.Alpha;
+using Amazon.CDK.AWS.DynamoDB;
+using Amazon.CDK.AWS.IAM;
+using Amazon.CDK.AWS.Lambda;
 using IYLTDSU.Signalling.Infrastructure;
+using Attribute = Amazon.CDK.AWS.DynamoDB.Attribute;
 
 // ReSharper disable InconsistentNaming
 var app = new App();
@@ -11,8 +17,28 @@ var stack = new Stack(app, WickedSickService.GetIdentifierFor(nameof(Stack)), ne
     Env = new Amazon.CDK.Environment { Account = "561462764262", Region = "eu-west-1" }
 });
 
-new FunctionConstruct(stack, "OnConnect");
-new FunctionConstruct(stack, "Disconnect");
-new FunctionConstruct(stack, "OnMessage");
+new SignallingConstruct(stack, "Signalling");
+
+// var Principal = new ServicePrincipal("apigateway.amazonaws.com");
+// var Role = new Role(stack, "WebSocketRole", new RoleProps
+// {
+//     RoleName = "websockets-role",
+//     AssumedBy = Principal
+// });
+// func.OnConnect.GrantInvoke(Role);
+
+// var stringBuilder = new System.Text.StringBuilder();
+// stringBuilder.Append($"/{Constants.Prefix}");
+// stringBuilder.Append($"/{Constants.Flyingdarts}");
+// stringBuilder.Append($"/{System.Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}");
+// stringBuilder.Append($"/{nameof(WebSocketApi)}");
+// stringBuilder.Append("/DynamoDb");
+//
+// var stringParam = new StringParameter(stack, "StringParam", new StringParameterProps
+// {
+//     ParameterName = stringBuilder.ToString(),
+//     StringValue = table.TableName
+// });
+//
 
 app.Synth();
