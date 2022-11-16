b19d15597e5f0c3b2d610a35f4f96d9793ef20f2
Mike Pattyn <mike.pattyn1@gmail.com>
2022-11-16 10:14:02 +0100

    Checkin new lambdas for rooms handling

---
+++ IYLTDSU.Signalling.Rooms.OnCreate/Function.cs
@@ -0,0 +1,37 @@
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.RuntimeSupport;
+using Amazon.Lambda.Serialization.SystemTextJson;
+
+var DynamoDbClient = new AmazonDynamoDBClient();
+var TableName = Environment.GetEnvironmentVariable("TableName")!;
+var ConnectionIdField = "ConnectionId";
+var RoomIdField = "RoomId";
+
+// The function handler that will be called for each Lambda event
+var handler = async (APIGatewayProxyRequest request, ILambdaContext context) =>
+{
+    var connectionId = request.RequestContext.ConnectionId;
+    var roomId = Guid.Parse(request.Body);
+
+    var putItemRequest = new PutItemRequest
+    {
+        TableName = TableName,
+        Item = new Dictionary<string, AttributeValue>
+        {
+            { ConnectionIdField, new AttributeValue{ S = connectionId } },
+            { RoomIdField, new AttributeValue{ S = roomId.ToString().ToLower() } }
+        }
+    };
+
+    await DynamoDbClient.UpdateItemAsync(putItemRequest);
+};
+
+// Build the Lambda runtime client passing in the handler to call for each
+// event and the JSON serializer to use for translating Lambda JSON documents
+// to .NET types.
+await LambdaBootstrapBuilder.Create(handler, new DefaultLambdaJsonSerializer())
+        .Build()
+        .RunAsync();
