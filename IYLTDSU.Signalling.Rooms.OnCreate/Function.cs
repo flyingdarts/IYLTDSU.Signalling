cbb47e1447f4babd08577bd8ccf03472b4c0c9ab
Mike Pattyn <mike.pattyn1@gmail.com>
2022-11-26 11:41:30 +0100

    Change property name to message and get roomId from body object

--- IYLTDSU.Signalling.Rooms.OnCreate/Function.cs
+++ IYLTDSU.Signalling.Rooms.OnCreate/Function.cs
@@ -4,6 +4,8 @@ using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.RuntimeSupport;
 using Amazon.Lambda.Serialization.SystemTextJson;
+using System.Net;
+using System.Text.Json;
 
 var DynamoDbClient = new AmazonDynamoDBClient();
 var TableName = Environment.GetEnvironmentVariable("TableName")!;
@@ -13,20 +15,49 @@ var RoomIdField = "RoomId";
 // The function handler that will be called for each Lambda event
 var handler = async (APIGatewayProxyRequest request, ILambdaContext context) =>
 {
-    var connectionId = request.RequestContext.ConnectionId;
-    var roomId = Guid.Parse(request.Body);
-
-    var putItemRequest = new PutItemRequest
+    try
     {
-        TableName = TableName,
-        Item = new Dictionary<string, AttributeValue>
+        var connectionId = request.RequestContext.ConnectionId;
+        JsonDocument message = JsonDocument.Parse(request.Body);
+        JsonElement dataProperty;
+        if (!message.RootElement.TryGetProperty("message", out dataProperty) || dataProperty.GetString() == null)
+        {
+            context.Logger.LogInformation("Failed to find data element in JSON document");
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = (int)HttpStatusCode.BadRequest
+            };
+        }
+
+        var roomId = Guid.Parse(dataProperty.GetString()!);
+
+        var putItemRequest = new PutItemRequest
+        {
+            TableName = TableName,
+            Item = new Dictionary<string, AttributeValue>
         {
             { ConnectionIdField, new AttributeValue{ S = connectionId } },
             { RoomIdField, new AttributeValue{ S = roomId.ToString().ToLower() } }
         }
-    };
+        };
 
-    await DynamoDbClient.PutItemAsync(putItemRequest);
+        await DynamoDbClient.PutItemAsync(putItemRequest);
+        return new APIGatewayProxyResponse
+        {
+            StatusCode = (int)HttpStatusCode.Created,
+            Body = "Room Created"
+        };
+    }
+    catch (Exception e)
+    {
+        context.Logger.LogInformation("Error disconnecting: " + e.Message);
+        context.Logger.LogInformation(e.StackTrace);
+        return new APIGatewayProxyResponse
+        {
+            StatusCode = (int)HttpStatusCode.InternalServerError,
+            Body = $"Failed to send message: {e.Message}"
+        };
+    }
 };
 
 // Build the Lambda runtime client passing in the handler to call for each
