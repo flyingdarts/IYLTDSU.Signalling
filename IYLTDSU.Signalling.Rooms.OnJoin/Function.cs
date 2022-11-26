1f968b29415352d299c0e367f21035545ed35add
Mike Pattyn <mike.pattyn1@gmail.com>
2022-11-26 13:41:31 +0100

    Join a room

--- IYLTDSU.Signalling.Rooms.OnJoin/Function.cs
+++ IYLTDSU.Signalling.Rooms.OnJoin/Function.cs
@@ -1,13 +1,65 @@
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.RuntimeSupport;
 using Amazon.Lambda.Serialization.SystemTextJson;
+using System.Net;
+using System.Text.Json;
+
+var DynamoDbClient = new AmazonDynamoDBClient();
+var TableName = Environment.GetEnvironmentVariable("TableName")!;
+var ConnectionIdField = "ConnectionId";
+var RoomIdField = "RoomId";
 
 // The function handler that will be called for each Lambda event
-var handler = (string input, ILambdaContext context) =>
+var handler = async (APIGatewayProxyRequest request, ILambdaContext context) =>
 {
-    return input.ToUpper();
-};
+    try
+    {
+        var connectionId = request.RequestContext.ConnectionId;
+        JsonDocument message = JsonDocument.Parse(request.Body);
+        JsonElement dataProperty;
+        if (!message.RootElement.TryGetProperty("message", out dataProperty) || dataProperty.GetString() == null)
+        {
+            context.Logger.LogInformation("Failed to find data element in JSON document");
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = (int)HttpStatusCode.BadRequest
+            };
+        }
 
+        var roomId = Guid.Parse(dataProperty.GetString()!);
+
+        var putItemRequest = new PutItemRequest
+        {
+            TableName = TableName,
+            Item = new Dictionary<string, AttributeValue>
+        {
+            { ConnectionIdField, new AttributeValue{ S = connectionId } },
+            { RoomIdField, new AttributeValue{ S = roomId.ToString().ToLower() } }
+        }
+        };
+
+        await DynamoDbClient.PutItemAsync(putItemRequest);
+
+        return new APIGatewayProxyResponse
+        {
+            StatusCode = (int)HttpStatusCode.Created,
+            Body = "Room Created"
+        };
+    }
+    catch (Exception e)
+    {
+        context.Logger.LogInformation("Error disconnecting: " + e.Message);
+        context.Logger.LogInformation(e.StackTrace);
+        return new APIGatewayProxyResponse
+        {
+            StatusCode = (int)HttpStatusCode.InternalServerError,
+            Body = $"Failed to send message: {e.Message}"
+        };
+    }
+};
 // Build the Lambda runtime client passing in the handler to call for each
 // event and the JSON serializer to use for translating Lambda JSON documents
 // to .NET types.
